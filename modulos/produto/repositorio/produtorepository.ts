05310c99aee9f1b7002f064e7dd5fff195efe72f
TBm1512 <108409645+TainaBrito@users.noreply.github.com>
2024-01-04 19:43:56 -0300

    Create produtorepository.ts

---
+++ modulos/produto/repositorio/produtorepository.ts
@@ -0,0 +1,9 @@
+import { ProdutoDto } from "../dtos/produto.dto";
+export interface ProdutoRepository {
+  
+  getAllProdutos(): Promise<ProdutoDto[]>;
+  createProduto(dto: ProdutoDto): Promise<ProdutoDto>;
+  getProduto(id: number): Promise<ProdutoDto | null>;
+  uto(dto: ProdutoDto): Promise<ProdutoDto | null>;
+  deleteProduto(id: number): Promise<ProdutoDto | null>;
+}
